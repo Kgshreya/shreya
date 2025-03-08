67fd8eb54d7a161d90d4b93911eb1a7bde999d4d
Kgshreya <shreyaguru2004@gmail.com>
2025-03-08 08:39:32 +0530

    Add files via upload

---
+++ updateuser.h
@@ -0,0 +1,13 @@
+//updateuser.h
+#ifndef UPDATEUSER_H
+#define UPDATEUSER_H
+
+#include "create.h"
+
+// Function declaration for updateUser
+void createUser(struct User *users,
+                int *userCount,
+                int userid,
+                );
+
+#endif
